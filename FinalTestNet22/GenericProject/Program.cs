6c7bd7ef2b8f68a4e570a52706942cf25c6f9eb6
Bala Pasala <balaanthony1219@gmail.com>
2021-01-18 20:30:31 -0500

    Final submission but needs little more corrections

--- FinalTestNet22/GenericProject/Program.cs
+++ FinalTestNet22/GenericProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GenericProject
 {
@@ -15,6 +16,14 @@ namespace GenericProject
 4)    Has a method that will return an  item from the list (e.g. item 3 or item 4 in the list)
 5)    Has a method that returns a sorted collection with the items in descending order
              * */
+            var ec = new ExampleClass<int>();
+            ec.AddItem(3);
+            ec.AddItem(2);
+            ec.AddItem(1);
+            Console.WriteLine($"Given order: {0},{1},{2}", ec.GetItem(0), ec.GetItem(1), ec.GetItem(2));
+            var desSortList = ec.SortedCollection().ToList();
+            Console.WriteLine($"Sorted : {0},{1},{2}", desSortList[0], desSortList[1], desSortList[2]);
+
         }
         public class ExampleClass<T> where T: struct
         {
@@ -23,7 +32,18 @@ namespace GenericProject
             {
                 privateList.Add(val);
             }
+            public T GetItem(int index)
+            {
+                return privateList[index];
+            }
 
+            public List<T> SortedCollection()
+            {
+                //IEnumerable<T> sc = privateList.OrderByDescending(x => x);                
+                // return sc;
+                privateList.Sort();
+                return privateList;
+            }
 
         }
     }
